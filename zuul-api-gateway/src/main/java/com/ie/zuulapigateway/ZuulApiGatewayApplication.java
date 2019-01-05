9cc212c083c83b0dbfd5b5d11c021d927663c271
İbrahim Ersin YAVAŞ <iersinyavas@gmail.com>
2019-01-05 23:29:21 +0300

    Zuul kütüphanesi ile mikroservis yönlendirmeleri

---
+++ zuul-api-gateway/src/main/java/com/ie/zuulapigateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.ie.zuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
+
